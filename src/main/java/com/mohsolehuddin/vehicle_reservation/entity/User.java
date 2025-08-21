c429ced587be4aa3c1ec0d1cbd7646a07b765992
Moh Solehuddin <moh.solehuddin190805@gmail.com>
2025-08-21 07:59:13 +0700

    FEAT: add user entity

---
+++ src/main/java/com/mohsolehuddin/vehicle_reservation/entity/User.java
@@ -0,0 +1,37 @@
+package com.mohsolehuddin.vehicle_reservation.entity;
+
+import com.mohsolehuddin.vehicle_reservation.constant.PathDB;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.JoinColumn;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+@Data
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+@Entity
+@Table(name = PathDB.USER)
+public class User extends AuditEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @ManyToOne
+    @JoinColumn(name = "role_id")
+    private Role role;
+}
