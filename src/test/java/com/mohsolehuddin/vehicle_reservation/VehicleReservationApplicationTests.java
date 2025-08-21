4b506aa6556d5418d2295c868e564251ac0c3636
Moh Solehuddin <moh.solehuddin190805@gmail.com>
2025-08-21 07:47:17 +0700

    init project

---
+++ src/test/java/com/mohsolehuddin/vehicle_reservation/VehicleReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mohsolehuddin.vehicle_reservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
