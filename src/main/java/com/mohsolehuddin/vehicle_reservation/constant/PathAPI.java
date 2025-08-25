a73ec23219375360f110fa22dc3b6edccf785552
Moh Solehuddin <moh.solehuddin190805@gmail.com>
2025-08-25 07:05:50 +0700

    FEAT: add path api constan fk auth and reservation

---
+++ src/main/java/com/mohsolehuddin/vehicle_reservation/constant/PathAPI.java
@@ -0,0 +1,9 @@
+package com.mohsolehuddin.vehicle_reservation.constant;
+
+public class PathAPI {
+    private static final String BASE_API_V1 = "/api/v1";
+    public static final String AUTH = BASE_API_V1 + "/auth";
+    public static final String SIGN_UP = "/signup";
+    public static final String SIGN_IN = "/signin";
+    public static final String RESERVATION = BASE_API_V1 + "reservation";
+}
