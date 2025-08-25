2cd066180320f3f3b942c2b615589b0ddf69649a
Moh Solehuddin <moh.solehuddin190805@gmail.com>
2025-08-25 07:11:10 +0700

    FEAT: add User entity

--- src/main/java/com/mohsolehuddin/vehicle_reservation/entity/User.java
+++ src/main/java/com/mohsolehuddin/vehicle_reservation/entity/User.java
@@ -2,15 +2,18 @@ package com.mohsolehuddin.vehicle_reservation.entity;
 
 import com.mohsolehuddin.vehicle_reservation.constant.PathDB;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 import jakarta.persistence.GenerationType;
-import jakarta.persistence.Column;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import lombok.*;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 @Data
