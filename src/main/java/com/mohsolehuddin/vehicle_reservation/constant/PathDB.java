c74f32f460aa8b250f96c4a7bd2ea07d7753b956
Moh Solehuddin <moh.solehuddin190805@gmail.com>
2025-08-25 11:35:09 +0700

    FEAT: add vechcle db path

--- src/main/java/com/mohsolehuddin/vehicle_reservation/constant/PathDB.java
+++ src/main/java/com/mohsolehuddin/vehicle_reservation/constant/PathDB.java
@@ -6,4 +6,6 @@ public class PathDB {
     public static final String USER = MASTER + "users";
     public static final String USER_PROFILE = MASTER + "user_profiles";
     public static final String ROLE = MASTER + "roles";
+    public static final String RESERVATION = MASTER + "reservations";
+    public static final String VEHICLE = MASTER + "vehicles";
 }
