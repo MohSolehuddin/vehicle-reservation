ef14063bfa6b461e9c048096132379d9e05d1e26
Moh Solehuddin <moh.solehuddin190805@gmail.com>
2025-08-25 07:08:02 +0700

    FEAT: add constant user level

---
+++ src/main/java/com/mohsolehuddin/vehicle_reservation/constant/UserLevel.java
@@ -0,0 +1,6 @@
+package com.mohsolehuddin.vehicle_reservation.constant;
+
+public class UserLevel {
+    public static final String ADMIN = "hasRole('ADMIN')";
+    public static final String STAFF = "hasRole('APPROVER')";
+}
