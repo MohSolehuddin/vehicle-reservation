8f706a05f0af6c2ee397bf59c344c13c35001c19
Moh Solehuddin <moh.solehuddin190805@gmail.com>
2025-08-25 07:13:24 +0700

    FEAT: add user repository

---
+++ src/main/java/com/mohsolehuddin/vehicle_reservation/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.mohsolehuddin.vehicle_reservation.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.mohsolehuddin.vehicle_reservation.entity.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+    Optional<User> findByEmail(String email);
+}
