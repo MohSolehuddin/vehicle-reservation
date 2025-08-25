cd780657b83dbe7428e9bf3d62121e98e9c3302d
Moh Solehuddin <moh.solehuddin190805@gmail.com>
2025-08-25 07:07:20 +0700

    FEAT: add constant path db for roles, users, user profiles

---
+++ src/main/java/com/mohsolehuddin/vehicle_reservation/constant/PathDB.java
@@ -0,0 +1,9 @@
+package com.mohsolehuddin.vehicle_reservation.constant;
+
+public class PathDB {
+    public static final String MASTER = "m_";
+    public static final String TRANSACTION = "t_";
+    public static final String USER = MASTER + "users";
+    public static final String USER_PROFILE = MASTER + "user_profiles";
+    public static final String ROLE = MASTER + "roles";
+}
